04ef03f29f5c1fd3c1674776bfacb9abdfe38ab0
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-08-07 10:42:44 +0200

    izmjena

--- dodavanjeKlijenata/View.php
+++ dodavanjeKlijenata/View.php
@@ -3,7 +3,7 @@
 sticenjeStranice($putanjaAplikacije);
 ?>
 <?php
-$conn = mysqli_connect("Localhost","root","","records");
+$conn = mysqli_connect("Localhost","root","","crud");
 
 ?>
 <?php 
@@ -16,44 +16,83 @@ $conn = mysqli_connect("Localhost","root","","records");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" a href="CSS/bootstrap.css"/>
-    <title>View Records</title>
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+     <link rel="stylesheet" href="../assets/css/foundation.css" />
+     <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Prijava</title>
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
 </head>
-<body class="bg-dark">
+<body>
 
-        <div class="container">
-            <div class="row">
-                <div class="col m-auto">
-                    <div class="card mt-5">
-                        <table class="table table-bordered">
+<div class="top-bar">
+  <div class="top-bar-left">
+    <ul class="dropdown menu" data-dropdown-menu>
+      <li class="menu-text">Auto servis</li>
+      <a href="../index.php">Početna</a>
+      <li class="has-submenu">
+        <a href="#0">Usluge</a>
+        <ul class="submenu menu vertical" data-submenu>
+          <li><a href="#0">Punjenje klima</a></li>
+          <li><a href="#0">Mehanika</a></li>
+          <li><a href="#0">Autoelektrika/Diagnostika</a></li>
+        </ul>
+        <li><a href="../dodavanjeKlijenata\index.php">admin</a></li>
+      </li>
+      <a href="#">Dobrodošli</a><p></p><a class="odjava" href="../odjava.php">Odjavi me </a></ul>
+  </div>
+  <div class="top-bar-right">
+    <ul class="menu">
+      <li><input type="search" placeholder="Search"></li>
+      <li><button type="button" class="button">Search</button></li>
+    </ul>
+  </div>
+</div>
+<?php echo $_SESSION['autoriziran'] ?>
+<div class="grid-container">
+        <div class="callout pocetna">
+            <a href="./insert.php" class="button">Dodaj novi</a>
+            <div class="table-scroll">
+            <table class="hover">
                             <tr>
-                                <td> User ID </td>
-                                <td> User Name </td>
-                                <td> User Email </td>
-                                <td> User Age </td>
-                                <td> Edit  </td>
-                                <td> Delete </td>
+                            <td> Klijent ID </td>
+                                <td> Klijent Ime </td>
+                                <td> Klijent Prezime </td>
+                                <td> Usluga </td>
+                                <td> Kontakt  </td>
+                                <td> Email </td>
+                                <td> Adresa stanovanja </td>
+                                <td> Datum dolaska</td>
+                                <td> Izmjena </td>
+                                <td> brisanje </td>
                             </tr>
 
                             <?php 
                                     
                                     while($row=mysqli_fetch_assoc($result))
                                     {
-                                        $UserID = $row['User_ID'];
-                                        $UserName = $row['User_Name'];
-                                        $UserEmail = $row['User_Email'];
-                                        $UserAge = $row['User_Age'];
+                                        $Korisnik_ID = $row['Korisnik_ID'];
+                                        $Korisnik_Ime = $row['Korisnik_Ime'];
+                                        $Korisnik_prezime = $row['Korisnik_prezime'];
+                                        $Korisnik_usluga = $row['Korisnik_usluga'];
+                                        $Korisnik_kontakt = $row['Korisnik_kontakt'];
+                                        $Korisnik_email = $row['Korisnik_email'];
+                                        $Korisnik_adresa = $row['Korisnik_adresa'];
+                                        $Korisnik_dolazak = $row['Korisnik_dolazak'];
                             ?>
                                     <tr>
-                                        <td><?php echo $UserID ?></td>
-                                        <td><?php echo $UserName ?></td>
-                                        <td><?php echo $UserEmail ?></td>
-                                        <td><?php echo $UserAge ?></td>
-                                        <td><a href="edit.php?GetID=<?php echo $UserID ?>">Edit</a></td>
-                                        <td><a href="delete.php?Del=<?php echo $UserID ?>">Delete</a></td>
+                                    <td><?php echo $Korisnik_ID ?></td>
+                                        <td><?php echo $Korisnik_Ime ?></td>
+                                        <td><?php echo $Korisnik_prezime ?></td>
+                                        <td><?php echo $Korisnik_usluga ?></td>
+                                        <td><?php echo $Korisnik_kontakt ?></td>
+                                        <td><?php echo $Korisnik_email ?></td>
+                                        <td><?php echo $Korisnik_adresa ?></td>
+                                        <td><?php echo $Korisnik_dolazak ?></td>
+                                        <td><a class="button warning" href="edit.php?ID=<?php echo $Korisnik_ID ?>">Edit</a></td>
+                                        <td><a class="button alert" href="delete.php?Del=<?php echo $Korisnik_ID ?>">Delete</a></td>
                                     </tr>        
                             <?php 
                                     }  
@@ -61,6 +100,7 @@ $conn = mysqli_connect("Localhost","root","","records");
                                    
 
                         </table>
+                        
                     </div>
                 </div>
             </div>
